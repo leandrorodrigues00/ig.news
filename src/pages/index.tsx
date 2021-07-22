190ff603751c00fc51296723cf9532b169f1fed2
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2021-07-21 23:02:21 -0300

    adding stripe api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,60 @@
+import { GetServerSideProps } from "next";
+
 import Head from "next/head";
-import {SubscribeButton} from '../components/SubscribeButton';
+import { SubscribeButton } from "../components/SubscribeButton";
 
+import { stripe } from "../services/stripe";
 
-import styles from './home.module.scss';
+import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product:{
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }:HomeProps) {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
       <main className={styles.contentContainer}>
-        <section className={styles.hero}> 
+        <section className={styles.hero}>
           <span> 👏 Hey, welcome</span>
-          <h1> News about the <span>React</span> world</h1>
+          <h1>
+            {" "}
+            News about the <span>React</span> world
+          </h1>
           <p>
-            Get access to all the publications <br/>
-            <span>for $9.90 month </span>
-
+            Get access to all the publications <br />
+            <span>for {product.amount} month </span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
-        <img src="/images/avatar.svg" alt="Girl coding"  />
-
-
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JFqLMLmHkChQfwaDzLAJic0");
+
+  const product = {
+    priceID: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+       style: 'currency',
+       currency:"USD",
+
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
