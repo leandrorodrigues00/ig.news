7c7c1afce96f2c5bbdbff56dabae531d12ef63a4
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2021-07-22 22:55:33 -0300

    add apiRoutes and faunaDB

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -6,6 +6,13 @@ import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
+
+
+// Client-side
+// Server-side
+// Static Site Generation 
+
+
 
 interface HomeProps {
   product:{
@@ -40,7 +47,7 @@ export default function Home({ product }:HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1JFqLMLmHkChQfwaDzLAJic0");
 
   const product = {
@@ -56,5 +63,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
